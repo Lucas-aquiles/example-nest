89093810102030e0b54c26ff655a621d77be0cd2
agusmottes <amottes@wecom.global>
2024-07-31 16:18:52 -0300

    responsive Introduction

--- components/Introducttion.tsx
+++ components/Introducttion.tsx
@@ -9,23 +9,28 @@ const Introducttion = ({data}: any) => {
   };
 
   return (
-    <div className="inset-0 h-auto bg-black	 ">
-      <div className=" mx-auto flex w-3/4  flex-row items-center ">
-        <ImageContainer
-          image={image}
-          className="  h-[150px] w-full  sm:h-[500px] sm:w-[500px]"
-          imageClassName="object-cover rounded-xl" // Ajusta la imagen para cubrir el contenedor
+    <div className="inset-0 h-auto bg-black">
+  <div className="mx-auto flex flex-col sm:flex-row w-full sm:w-3/4 items-center">
+    <ImageContainer
+      image={image}
+      className="h-[150px] w-[80%] sm:h-[500px] sm:w-[400px] my-4 sm:my-8 sm:mr-12"
+      imageClassName="object-cover rounded-xl"
+      priority
+    />
 
-          priority
-        />
-
-        <div className="  mx-auto  w-2/5  ">
-          <h2 className="text-4xl text-blue-50">{data.title}</h2>
-          <br />
-          <p className="text-gray-400">{data.content}</p>
-        </div>
-      </div>
+    <div className="w-full sm:w-2/5 text-center sm:text-left px-4 sm:px-0">
+      <h2 className="text-2xl sm:text-4xl text-blue-50">{data.title}</h2>
+      <br />
+      <p className="text-gray-400">{data.content}</p>
+      <hr className="mt-6 sm:mt-8 border-gray-600" />
     </div>
+  </div>
+</div>
+
+
+
+  
+  
   );
 };
 
